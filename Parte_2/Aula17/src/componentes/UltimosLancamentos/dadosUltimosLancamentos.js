fbc260f9ad6e35c2b413bfab09179f5e247095ae
Alexandre Faria <132949575+alefaria577@users.noreply.github.com>
2024-10-15 09:55:29 -0300

    add dia 17

---
+++ Parte_2/Aula17/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,8 @@
+import ProgressiveWebApps from '../../imagens/Progressive Web Apps.png'
+import Sass from '../../imagens/Sass.png'
+import TurbineSeuCSS from '../../imagens/Turbine seu CSS.png'
+export const livros = [
+    { nome: 'Progressive Web Apps', id: 9, src: ProgressiveWebApps },
+    { nome: 'Sass', id: 10, src: Sass },
+    { nome: 'Turbine seu CSS', id: 11, src: TurbineSeuCSS },
+]
